3a4a83fb9b4cd4a67391b38e5b062ff0f87b83a5
sarasarwar07 <sarasarwar2001@gmail.com>
2025-08-28 03:53:02 +0600

    added copy text and copy count function

--- script.js
+++ script.js
@@ -8,6 +8,31 @@ function favoriteCount() {
       favoriteCounter.textContent = currentFavoriteCounter;
     }
 
+function copyCount(){
+    const copyCounter= document.getElementById('copy-count')
+    let currentCopyCounter = parseInt(copyCounter.textContent)
+    currentCopyCounter++
+    copyCounter.textContent= currentCopyCounter
+}
+
+function copyServiceNumber(id){
+    const copyText= document.getElementById(id).innerText
+    return navigator.clipboard.writeText(copyText).then(() => {
+        alert("Copied number: " + copyText);
+      })
+    
+}
+
+document.querySelectorAll('.copy-btn').forEach(btn => {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.dataset.target;  
+    copyServiceNumber(targetId);
+  });
+});
+
+
+
 
 
 
